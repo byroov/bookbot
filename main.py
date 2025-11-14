c0027e091ba1b73c7706ff93c60356a8412694fb
byrooski <byronverheyen@gmail.com>
2025-11-13 22:04:41 -0500

    add get_wordcount func

--- main.py
+++ main.py
@@ -4,8 +4,17 @@ def get_book_text(path):
     with open(path) as f:
         file = f.read()
         return file
+    
 def main(path):
-    print(get_book_text(path))
+    text_str = get_book_text(path)
+    print(text_str)
+
+text = get_book_text(path)
+
+def get_wordcount(text):
+   num_words =len(text.split())
+   print(f"Found {num_words} total words")
 
 
-main(path)
+
+get_wordcount(text)
